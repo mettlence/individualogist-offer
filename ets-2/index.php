50e0e7a92d9ca28d169d6394b00d8ae45957ce00
rohmaddj <rohmaddj@gmail.com>
2026-03-25 14:02:13 +0700

    fix

--- ets-2/index.php
+++ ets-2/index.php
@@ -17,15 +17,15 @@ include_once($path);
   .strong {font-weight:700}
   .sp-title{line-height:1.2em;font-size:1.75em;text-transform:uppercase;letter-spacing:2px;font-weight:900;text-align: center}
   .bg {position:relative;background-size:cover;background-attachment:fixed;background-position:center;background-repeat:none}
-  .squaresphere{background-image:url('https://individualogist.com/offer/assets/img/bg-squaresphere.jpg');border-radius:0 0 20% 20%;box-shadow:0 5px 15px rgba(0,0,0,.3);}
-  .meditateneon{position:relative;background-image:url('https://individualogist.com/offer/assets/img/bg-meditateneon.jpg');padding-top:2em;padding-bottom:2em;color:#fff;text-shadow:1px 1px 1px #000}
+  .squaresphere{background-image:url('/offer/assets/img/bg-squaresphere.jpg');border-radius:0 0 20% 20%;box-shadow:0 5px 15px rgba(0,0,0,.3);}
+  .meditateneon{position:relative;background-image:url('/offer/assets/img/bg-meditateneon.jpg');padding-top:2em;padding-bottom:2em;color:#fff;text-shadow:1px 1px 1px #000}
   .meditateneon p{font-size:1.5em}
   .overlay-h{display:block;background:linear-gradient(rgba(0, 0, 0, 0.75),rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.75));top:0;left:0;bottom:0;width:100%;height:auto}
   .overlay-hero{display:block;background:linear-gradient(rgba(0, 0, 0, 0.75),rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.75));top:0;left:0;bottom:0;width:100%;height:auto;border-radius:0 0 20% 20%}
   .hero{padding-top:6em;padding-bottom:6em;color:#fff;text-shadow:1px 1px 1px #000;}
   .bg-black{background:#000}.bg-darker{background:#222}
   .text-white{color:#fff}
-  .magicspark {font-size:1.2em;background-position:center;background-size:cover;width:100%;background-image:url('https://individualogist.com/offer/assets/img/bg-magicspark.jpg');position:relative;padding:2em}
+  .magicspark {font-size:1.2em;background-position:center;background-size:cover;width:100%;background-image:url('/offer/assets/img/bg-magicspark.jpg');position:relative;padding:2em}
   .pa{position:absolute}
   .overlay-v{display:block;background:linear-gradient(to right,#000,rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0));top:0;left:0;bottom:0;width:100%;height:auto}
   .cb{clear:both}
@@ -38,7 +38,7 @@ include_once($path);
   .bg-cyan{background:#69eed8;}
   .ul-crystal{list-style:none;margin-left:1em;text-align:justify;margin-bottom:20px}
   .ul-crystal > li {position: relative;margin-bottom: 5px;margin-left: 10px;}
-  .ul-crystal > li:before {position: absolute;left: -40px;display: inline-block;width: 22px;height: 20px;margin-right: 1em;background: url(https://individualogist.com/offer/assets/img/crystal-icon.png) no-repeat;background-size: cover;background-position: 100% 100%;border-radius: 50%;content: "";top: 5px;}
+  .ul-crystal > li:before {position: absolute;left: -40px;display: inline-block;width: 22px;height: 20px;margin-right: 1em;background: url(/offer/assets/img/crystal-icon.png) no-repeat;background-size: cover;background-position: 100% 100%;border-radius: 50%;content: "";top: 5px;}
   .smallprint{text-align:center;font-size:.8em;color:#333;font-weight:700;padding-top:.5em;color:red;opacity:.8}
   .ets{width:100%;max-width:600px;display:block;margin:1em auto}
   .ets2{right:0;max-width:250px}
@@ -46,19 +46,19 @@ include_once($path);
   .stone-comparison{width:100%;position:relative;display:block;margin: 1em auto;padding:1em}
   .chakra{width:80%;max-width:600px;position:relative;display:block;margin: 1em auto;padding:1em}
   .quote-card{background:#f8f8f8;padding:1em 2em;width:100%;max-width:500px;display:block;position:relative;box-shadow:0 8px 15px rgba(0,0,0,.15);border-radius:.5em;margin:2em auto}
-  .stars{width:100px;height:30px;background:url('https://individualogist.com/offer/assets/img/5stars.png');background-size:contain;background-repeat:no-repeat;display:block;position:relative;margin: 0 auto}
+  .stars{width:100px;height:30px;background:url('/offer/assets/img/5stars.png');background-size:contain;background-repeat:no-repeat;display:block;position:relative;margin: 0 auto}
   .quote-name{font-size: 14px;font-weight: 700;text-transform: uppercase;letter-spacing: 2px;color: #666;}
   .quote-text{text-align:center;color:#333}
   .quote-title{text-align:center;letter-spacing:2px;text-transform:uppercase;color:#666;font-weight:900}
-  .indi-logo{position:relative;height:100px;width:200px;background:url('https://individualogist.com/offer/assets/img/individualogist-logo2.png');margin: 0 auto;display:block;background-size:contain;background-repeat:no-repeat}
-  .multistep{position:relative;height:86.5px;width:100%;max-width:1024px;background:url('https://individualogist.com/offer/assets/img/multistep.png');margin: 0 auto;display:block;background-size:contain;background-repeat:no-repeat}
+  .indi-logo{position:relative;height:100px;width:200px;background:url('/offer/assets/img/individualogist-logo2.png');margin: 0 auto;display:block;background-size:contain;background-repeat:no-repeat}
+  .multistep{position:relative;height:86.5px;width:100%;max-width:1024px;background:url('/offer/assets/img/multistep.png');margin: 0 auto;display:block;background-size:contain;background-repeat:no-repeat}
   .p-0{padding:0 !important}
   .warning-title{color: red;font-weight: 700;font-size: 1.5em!important;margin-bottom: 0;margin-top: .5em;}
   .warning-text{margin:.5em auto;color:#000;font-weight:700;font-size:18px !important}
-  .alert-icon{background:url('https://individualogist.com/clickbank/assets/img/alert-icon.png');width:100px;height:100px;margin:0 auto;display:block;background-size:contain;background-repeat:no-repeat;}
+  .alert-icon{background:url('/offer/assets/img/alert-icon.png');width:100px;height:100px;margin:0 auto;display:block;background-size:contain;background-repeat:no-repeat;}
   .decline-link{color:#666;text-decoration:underline;text-align:center;margin:0 auto;font-size:14px}
   .quote-box{background:#222;box-shadow:0 5px 15px rgba(0,0,0,.15);padding:1em;max-width:700px;display:block;position:relative;margin: 1em auto;border-radius:.5em;color:#fff}
-  .headline-bg{max-width:1024px;margin: 0 auto;position:relative;background:url('https://individualogist.com/offer/assets/img/bg-cosmospink.png');background-repeat:no-repeat;background-attachment:fixed;background-size:cover;background-position:center center;padding: 1em;color:#fff;text-shadow:1px 1px 1px #000;width:100%}
+  .headline-bg{max-width:1024px;margin: 0 auto;position:relative;background:url('/offer/assets/img/bg-cosmospink.png');background-repeat:no-repeat;background-attachment:fixed;background-size:cover;background-position:center center;padding: 1em;color:#fff;text-shadow:1px 1px 1px #000;width:100%}
   .bg-white{background:#fff} .p-1{padding:1em !important}
   .upsell-hl{font-weight:700}
   @media only screen and (max-width:768px) {.buy-button{font-size:1em}.img-fr{width:100%;position:relative;margin:0 auto}p{clear:both}.atom,.molecule{width:100%;max-width:100%}.sp-title{font-size:1.2em}.upsell-hl{font-size:1.5em}.ets2{display:none}.quote-card{margin:1em auto}.warning-container{text-align:center}.hero-h1{font-size:1.5em}.hero-h4{font-size:1.2em}.alert-icon{width:50px;height:50px}}
@@ -115,7 +115,7 @@ include_once($path);
               <p>I've set the limit on this time-sensitive offer to just 15 minutes.</p>
               <p class="text-center strong">Are you going to do everything within your power to ensure the divine, energetic protection of those around you?</p>
               <p class="text-center strong">LIMITED OFFER: <strike>139.90</strike> $79.90 for TWO Additional Spheres!</p>
-              <img src="https://individualogist.com/clickbank/assets/img/sphere-2s.png" class="ets">
+              <img src="/offer/assets/img/sphere-2s.png" class="ets">
               <a class='btn buy-button' href='http://ets-2.individua1.pay.clickbank.net/?cbskin=25048&cbur=a'>Yes! Add To My Order!</a>
               <a class='decline text-center mt-3 mb-3' href='http://ets-2.individua1.pay.clickbank.net/?cbskin=25048&cbur=d'>No thanks, I will forfeit this opportunity...</a>
               <p class="smallprint">Lock in your discount now and claim your 2 Energy Transfiguration Spheres for just $79.90!</p>
@@ -143,7 +143,7 @@ include_once($path);
 </div>
 </div>
 
-<script type="text/javascript" src="paa/js/custom.js"></script>
+<script type="text/javascript" src="/offer/paa/js/custom.js"></script>
 <script>
 window.onload = function () {
 var string_contain = 'upsell';
